25b90ccbbcd72ba454204f0a14e918c1f3e10438
lkurdy <kurdylais25@gmail.com>
2023-06-13 20:33:06 +0200

    inc

--- srcs/requirements/wordpress/tools/launch.sh
+++ srcs/requirements/wordpress/tools/launch.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
 sleep 10
-#if [ -e "wp-config.php" ]; then
-#	exit 1
-#fi
-wp config create	--allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+wp config create	--allow-root \
+					--dbname=$SQL_DATABASE \
+					--dbuser=$SQL_USER \
+					--dbpass=$SQL_PASSWORD \
+					--dbhost=mariadb:3306 --path='/var/www/wordpress'
 
 wp core install --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
 
